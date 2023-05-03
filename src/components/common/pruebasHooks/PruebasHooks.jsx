3f7d4857bb89f007c0c3beda90b8ab5e09f7cd5f
Martin Vidal Freyre <martinvidal.n7@gmail.com>
2023-05-03 20:06:49 -0300

    fc2

--- src/components/common/pruebasHooks/PruebasHooks.jsx
+++ src/components/common/pruebasHooks/PruebasHooks.jsx
@@ -1,14 +1,29 @@
 // rafce - fafc
-import {useState} from "react"
-const PruebasHooks = () => {
-    
-    const [contador , setContador] = useState(0)
+import {useState, useEffect} from "react"
 
-
+const PruebasHooks = ({contador, setContador}) => {
     
+    const [img, setImg] = useState("")
+
+    let arr = [
+        "https://upload.wikimedia.org/wikipedia/en/5/5a/Grand_Moff_Tarkin.png",
+        "https://upload.wikimedia.org/wikipedia/en/5/5a/Grand_Moff_Tarkin.png",
+        "https://upload.wikimedia.org/wikipedia/en/5/5a/Grand_Moff_Tarkin.png"]
+
+    useEffect(() => {
+        setImg(arr[contador])
+    },[contador])
+
     return (
     <div>
         <h4>{contador}</h4>
+        <button onClick={()=> {setContador(contador +1)}}>Aumentar</button>
+        <button onClick={()=> {setContador(contador -1)}}>Disminuir</button>
+        <button onClick={()=> {setContador(0)}}>Reiniciar</button>
+
+        {/* <button onClick={() => setImg("https://upload.wikimedia.org/wikipedia/en/5/5a/Grand_Moff_Tarkin.png")}>Traer Imagen</button> */}
+        {/* <button onClick={() => setImg("")}>Quitar Imagen</button> */}
+        <img src={img} alt="" />
     </div>
     )
 }
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../../common/navbar/Navbar"
 import PruebasHooks from "../../common/pruebasHooks/PruebasHooks"
+import {useState} from "react"
 
 export const Home = () => {
+    const [contador , setContador] = useState(0)  
     return (
-    <>       
+    <>      
         <Navbar />
-        <PruebasHooks />
+        <PruebasHooks contador={contador} setContador={setContador} />
     </>
     )
 }
